9acd60e521b401e46569601a292d685c57d5c55f
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2024-06-02 18:10:54 -0700

    t-shirt info section

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ console.log("linked")
 const nameField = document.getElementById("name");
 const jobRole = document.getElementById("other-job-role");
 const roleSelect = document.getElementById("title");
+const colorSelection = document.getElementById("color");
+const designSelection = document.getElementById("design");
 
 
 
@@ -18,7 +20,7 @@ jobRole.style.display = "none";
 roleSelect.addEventListener("click", () => {
     if (roleSelect.value == "other") {
         jobRole.style.display = "block";
-    } else if (roleSelect.value != "other") {
+    } else if (roleSelect.value !== "other") {
         jobRole.style.display = "none";
     }
 });
@@ -26,3 +28,21 @@ roleSelect.addEventListener("click", () => {
 
 
 // ********** T-SHIRT INFO SECTION **********
+colorSelection.disabled = true;
+
+designSelection.addEventListener("click", () => {
+    if (designSelection.value != "Select Theme") {
+        colorSelection.disabled = false;
+    };
+});
+
+colorSelection.addEventListener("click", () => {
+    if (designSelection.value = "Theme - JS Puns") {
+
+    }
+})
+
+console.log(colorSelection.childNodes)
+console.log(colorSelection.childNodes[2].hidden = true)
+
+colorSelection.childNodes[2].hidden = true;
