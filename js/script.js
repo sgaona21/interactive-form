85d1b04644d9d4fe5bf37d87c860b314b19dd1f4
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2024-06-02 17:32:30 -0700

    JOB ROLE SECTION

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 console.log("linked")
 
-/********** VARIABLES **********/
-
+// ********** VARIABLES **********
 const nameField = document.getElementById("name");
+const jobRole = document.getElementById("other-job-role");
+const roleSelect = document.getElementById("title");
 
 
 
-
+// ********** NAME FIELD **********
 nameField.focus();
+
+
+
+// ********** JOB ROLE SECTION **********
+jobRole.style.display = "none";
+
+roleSelect.addEventListener("click", () => {
+    if (roleSelect.value == "other") {
+        jobRole.style.display = "block";
+    } else if (roleSelect.value != "other") {
+        jobRole.style.display = "none";
+    }
+});
+
+
+
+// ********** T-SHIRT INFO SECTION **********
